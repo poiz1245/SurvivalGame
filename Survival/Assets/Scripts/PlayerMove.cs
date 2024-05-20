b05efcb20be24f36de7fbab8005def0e8897bea5
poiz1245 <wva960@gmail.com>
2024-05-20 10:32:08 +0900

    feat : 플레이어 움직임 추가
    - topview 기준으로 플레이어 움직임
    - horizontal, viertical값을 받아서 rigidbody velocity로 움직임
    - 점프 x

---
+++ Survival/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using Unity.VisualScripting.FullSerializer;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class TopView : MonoBehaviour
+{
+
+    [SerializeField] float moveSpeed;
+
+    Rigidbody rigid;
+    float horizontalInput;
+    float verticaInput;
+
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticaInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 moveDir = new Vector3(horizontalInput, rigid.velocity.y, verticaInput);
+
+        Vector3 targetVelocity = moveDir * moveSpeed;
+        Vector3 velocityChange = (targetVelocity - rigid.velocity);
+
+        Movement(velocityChange);
+        Rotation(moveDir);
+    }
+
+    private void Rotation(Vector3 moveDir)
+    {
+        if (moveDir.x != 0 || moveDir.z != 0)
+        {
+            Quaternion deltaRotation = Quaternion.LookRotation(moveDir.normalized);
+            rigid.MoveRotation(deltaRotation);
+        }
+    }
+
+    private void Movement(Vector3 velocityChange)
+    {
+        rigid.AddForce(velocityChange, ForceMode.VelocityChange);
+    }
+}
