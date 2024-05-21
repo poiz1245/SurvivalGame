9e520bccf661ff4af2b016f373a8654ea02642ca
poiz1245 <wva960@gmail.com>
2024-05-21 11:46:50 +0900

    feat : 몬스터 풀에서 오브젝트 받아서 SpawnPoint에서 소환
    - 코루틴 사용

---
+++ Survival/Assets/Scripts/Spawner.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    //오브젝트 풀에서 몬스터 받아서 코루틴으로 몬스터 스폰
+    [SerializeField] int index;
+    [SerializeField] Transform spawnSpot;
+
+    void Start()
+    {
+        StartCoroutine(MonsterSpawn(0,1));
+    }
+    IEnumerator MonsterSpawn(int index, float spawnDelay)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnDelay);
+            GameObject monster = MonsterPool.GetMonster(index);
+            monster.transform.position = spawnSpot.position;
+        }
+    }
+}
