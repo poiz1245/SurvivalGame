8f02655ef76ea7f2368add8d8b64b931950de099
poiz1245 <wva960@gmail.com>
2024-05-22 10:25:18 +0900

    feat : 베지어 곡선에 사용하는 제3의 점 생성하는 구문 추가, 몬스터 충돌 처리
    - 총알과 타겟의 중간 지점을 구하고 거기에 y값을 더해서 3의 점 추가
    - 몬스터 충돌 시 데미지를 주고 사라짐

--- Survival/Assets/Scripts/Bullet.cs
+++ Survival/Assets/Scripts/Bullet.cs
@@ -8,21 +8,36 @@ public class Bullet : MonoBehaviour
 {
     Transform targetPos;
     Vector3 posA;
-
     float time = 0;
-
+    float maxDistance;
+    private void Start()
+    {
+        maxDistance = GameManager.Instance.player.scanRadius;
+    }
     void Update()
     {
         targetPos = GameManager.Instance.player.nearestTargetPos;
 
-        Vector3 dir = (targetPos.position-transform.position ).normalized;
-        posA = dir * 1f;
+        float distance = Vector3.Distance(transform.position, targetPos.position);
+        float offsetY = Mathf.Lerp(1f, 2f, distance / maxDistance);
 
-        print(posA);
-        Vector3 p1 = Vector3.Lerp(transform.position, posA, time);
-        Vector3 p2 = Vector3.Lerp(posA, targetPos.position, time);
+        Vector3 middlePoint = (transform.position + targetPos.position) / 2;
+        Vector3 thirdPoint = middlePoint + new Vector3(0, offsetY, 0);
+
+        Vector3 p1 = Vector3.Lerp(transform.position, thirdPoint, time);
+        Vector3 p2 = Vector3.Lerp(thirdPoint, targetPos.position, time);
 
         transform.position = Vector3.Lerp(p1, p2, time);
         time += Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Monster"))
+        {
+            Monster monster = other.gameObject.GetComponent<Monster>();
+            monster.GetDamage(GameManager.Instance.player.damage);
+            gameObject.SetActive(false);
+        }
     }
 }
